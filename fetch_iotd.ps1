ff7660b6e92192d60059e4178c8c6e4b7608a758
Jonathan Dickson <jonathan.p.dickson@gmail.com>
2020-09-01 08:24:18 -0500

    Some cleanup

--- fetch_iotd.ps1
+++ fetch_iotd.ps1
@@ -1,6 +1,29 @@
+Function Download-And-Save {
+    <#
+    .SYNOPSIS
+    This function will call the public API on NASA to fetch the image of the day, and save it to  %USERPROFILE%\OneDrive\Pictures\NASA
+
+    .RETURN
+    Returns the absolute path to the image that was saved.
+    #>
+
+    $request = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"
+    $user_image_path = "$env:USERPROFILE\OneDrive\Pictures\NASA"
+    
+    $apiresponse = Invoke-WebRequest $request | ConvertFrom-Json | Select hdurl, title
+    
+    $title = $apiresponse.title -replace ':',''
+    $hdurl = $apiresponse.hdurl
+    $image_absolute_path   ="$user_image_path\$title.jpg"
+
+    # The path is pretty standard, just change it if you want
+    Invoke-WebRequest $hdurl.toString() -OutFile $image_absolute_path
+
+    return $image_absolute_path
+}
+
 Function Set-WallPaper($Image) {
     <#
-     
         .SYNOPSIS
         Applies a specified wallpaper to the current user's desktop
         
@@ -36,39 +59,11 @@ Function Set-WallPaper($Image) {
      
 }
 
-
-# This is the API from NASA.
-# Check out all of their open APIs: https://api.nasa.gov/api.html  
-Function Download-And-Save {
-    <#
-    .SYNOPSIS
-    This function will call the public API on NASA to fetch the image of the day, and save it to  %USERPROFILE%\OneDrive\Pictures\NASA
-
-    .RETURN
-    Returns the absolute path to the image that was saved.
-    #>
-
-    $request     ="https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"
-    $user_image_path  ="$env:USERPROFILE\OneDrive\Pictures\NASA"
-    
-    $apiresponse =Invoke-WebRequest $request | ConvertFrom-Json | Select hdurl, title
-    
-    $title       =$apiresponse.title -replace ':',''
-    $hdurl       =$apiresponse.hdurl
-    $image_absolute_path   ="$user_image_path\$title.jpg"
-
-    # The path is pretty standard, just change it if you want
-    Invoke-WebRequest $hdurl.toString() -OutFile $image_absolute_path
-
-    return $image_absolute_path
+try {
+    $image_path = Download-And-Save
+    Set-WallPaper -Image $image_path
+} catch [InvokeMethodOnNull] {
+    "Invoke on null"
+} catch {
+    "Unknown exception"
 }
-
-$image_path = Download-And-Save
-Set-WallPaper -Image $image_path
-
-<#
-# set the wallpaper
-Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value "$image_path\$title.jpg"
-# Refresh the user properties.
-RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters ,1 ,True
-#>
